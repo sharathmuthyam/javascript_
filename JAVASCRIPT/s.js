e56e866ea5b536d8ccc42d8a179c463712473030
sharath muthyam <sharath_muthyam@Mac-1296.lan>
2025-02-15 21:16:44 -0600

    learned variables

---
+++ JAVASCRIPT/s.js
@@ -0,0 +1,9 @@
+    let admin,name;
+    const a="10.02.2002"
+
+    alert(someCode(a));
+    function someCode(a){
+        name = prompt("Enter your name");
+        
+        return name+"age is "+a;
+    }
